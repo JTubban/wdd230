faa992fe122500829feb0dc691beaab1006421a6
jtubban <nakoa024@gmail.com>
2024-02-09 13:52:30 +0800

    DOM manipulation

---
+++ lesson05/scripts/script.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+
+
+button.addEventListener('click', () => {
+    const outputElement = document.querySelector('#outputElement');
+    
+    const li = document.createElement('li');
+    li.textContent = input.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+
+    if (input.value === '') {
+        outputElement.textContent = 'Please enter a book and chapter.';
+        input.focus();    
+    }else{
+
+        li.append(deleteButton);
+        
+        list.appendChild(li);
+
+        outputElement.textContent = '';
+    }
+
+    deleteButton.addEventListener('click', () => {
+    list.removeChild(li);
+    input.focus();
+    input.value = '';
+});
+});
