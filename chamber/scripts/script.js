1f1e0aa127d8d05a77096249a39275d4742371ac
jtubban <nakoa024@gmail.com>
2024-02-10 07:56:45 +0800

    dark mode done

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,3 +5,45 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const cards = document.querySelectorAll(".card");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		cards.forEach(card => {
+			card.style.background = "#000";
+			card.style.color = "#fff";
+		});
+
+		header.style.background = "#000";
+		header.style.color = "#fff";
+
+		main.style.background = "gray";
+		main.style.color = "#fff";
+
+		footer.style.background = "#000";
+		footer.style.color = "#fff";
+
+		modeButton.textContent = "🔆";
+	} else {
+		cards.forEach(card => {
+			card.style.background = "gray";
+			card.style.color = "#000";
+		});
+
+		header.style.background = "#1D3557";
+		header.style.color = "#000";
+
+		main.style.background = "#eee";
+		main.style.color = "footer";
+
+		footer.style.background = "#1D3557";
+		footer.style.color = "#fff";
+
+		modeButton.textContent = "🕶️";
+	}
+});
