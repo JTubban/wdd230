74497e028799410383acefb5d7f99bf28807b65b
jtubban <nakoa024@gmail.com>
2024-02-26 08:30:36 +0800

    local storage

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -2,9 +2,6 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-
-
-
 button.addEventListener('click', () => {
     const outputElement = document.querySelector('#outputElement');
     
