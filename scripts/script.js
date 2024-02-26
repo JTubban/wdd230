e130093c70511928f8f8df467bfea18cad002ac4
jtubban <nakoa024@gmail.com>
2024-02-26 08:56:09 +0800

    Page Visits

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,18 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
     // map.classList.toggle('open');
 });
+
+// Page visits
+const visitsDisplay = document.querySelector('#pageVisits');
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = 'This is your first visit. Welcome!';
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-ls', numVisits);
