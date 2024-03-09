865d2c2035fce8845a2f3be3b4aa23be7badbc50
jtubban <nakoa024@gmail.com>
2024-03-09 21:58:28 +0800

    updated links

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,33 @@
 const baseURL = "https://jtubban.github.io/wdd230/";
 const linksURL = "https://jtubban.github.io/wdd230/data/links.json";
+// const linksURL = "../data/links.json";
+
+const activityList = document.querySelector("#activity-List");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
 
-    console.log(data);
+    displayLinks(data.lessons);
 }
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let li = document.createElement("li");
+        li.textContent = week.lesson;
+        
+        week.links.forEach((link) => {
+            let a = document.createElement("a");
+            a.textContent = link.title;
+
+            a.setAttribute("href", link.url);
+            a.setAttribute("target", "_blank");
+
+            li.appendChild(a);
+        });
+
+        activityList.appendChild(li);
+    });
+};
 
 getLinks();
