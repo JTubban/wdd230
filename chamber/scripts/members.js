c42834da19fe11a6b91e4877abbd02b09a369d2f
jtubban <nakoa024@gmail.com>
2024-03-12 22:54:15 +0800

    members.json updated

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,20 +1,29 @@
-// const grid = document.querySelector('.grid');
-const memData = 'members.json';
-
-// async function displayMembers(memData) {
-//     const response = await fetch(memData);
-//     const data = await response.json();
-
-//     console.log(data);
-// }
-
-async function getProphetData() {
-    const response = await fetch(memData);
+const url = 'https://jtubban.github.io/wdd230/chamber/data/members.json';
+const grid = document.querySelector('.grid');
+// grid.innerHTML = `<a href="https://www.google.com">james</a>`;
+async function getMembers() {
+    const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets);
-    displayProphets(data.prophets);
+
+    displayMembers(data.companies);
 }
 
-// displayMembers(memData);
+const displayMembers = (companies) => {
+    companies.forEach(company => {
+        grid.innerHTML = `
+            <section>
+                <img src="${company.image}" alt="Amazon Image">
+                <h3>${company.name}</h3>
+                <p>Redmond, Washington, USA</p>
+                <p>773-702-1000</p>
+                <p>CEO: Satya Nadella</p>
+                <p>Founded: 1975</p>
+                <p>Membership Level: Gold</p>
+                <p>Marker Cap: $3.029 Trillion</p>
+                <a href="https://www.amazon.com">Visit</a>
+            </section>
+        `;
+    });
+};
 
-getProphetData()
+getMembers();
