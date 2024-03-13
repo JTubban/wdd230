093fbb882aa8c5bca0f3e1561404b28414ca2f09
jtubban <nakoa024@gmail.com>
2024-03-13 14:52:27 +0800

    member.js added new script

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -54,5 +54,11 @@ const gridButton = document.querySelector('#grid');
 const listButton = document.querySelector('#list');
 
 gridButton.addEventListener('click', () => {
-    
+    display.classList.add('grid');
+    display.classList.remove('list');
+});
+
+listButton.addEventListener('click', () => {
+    display.classList.add('list');
+    display.classList.remove('grid');
 });
