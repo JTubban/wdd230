f1ef1f4a5999e801490ea10ded711f41eedd7e88
jtubban <nakoa024@gmail.com>
2024-03-18 23:23:53 +0800

    forecast

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,32 +1,32 @@
-// const currentDay = new Date().getDay();
+const currentDay = new Date().getDay();
 
-// function eventBanner(event) {
-//     if (event >= 1 && event <= 3) {
-//         const eventBanner = document.createElement('div');
-//         eventBanner.setAttribute('id', 'eventBanner');
+function eventBanner(event) {
+    if (event >= 1 && event <= 3) {
+        const eventBanner = document.createElement('div');
+        eventBanner.setAttribute('id', 'eventBanner');
     
-//         const pTag = document.createElement('p');
-//         pTag.textContent = 'Join us for the Chamber of Commerce meet and greet on Wednesday at 7:00pm. Thank you!';
+        const pTag = document.createElement('p');
+        pTag.textContent = 'Join us for the Chamber of Commerce meet and greet on Wednesday at 7:00pm. Thank you!';
         
-//         eventBanner.appendChild(pTag);
+        eventBanner.appendChild(pTag);
 
-//         // Add close button
-//         const closeButton = document.createElement('button');
-//         closeButton.setAttribute('class', 'closeButton');
-//         closeButton.textContent = '❌';
-//         closeButton.addEventListener('click', () => {
-//             eventBanner.style.display = "none";
-//         });
-//         eventBanner.append(closeButton);
+        // Add close button
+        const closeButton = document.createElement('button');
+        closeButton.setAttribute('class', 'closeButton');
+        closeButton.textContent = '❌';
+        closeButton.addEventListener('click', () => {
+            eventBanner.style.display = "none";
+        });
+        eventBanner.append(closeButton);
 
-//          // Add banner to page
-//         document.body.prepend(eventBanner);
-//     }
-// }
+         // Add banner to page
+        document.body.prepend(eventBanner);
+    }
+}
 
-// eventBanner(currentDay);
+eventBanner(currentDay);
 
-// Weather
+// // Weather
 const apiKey = '107e304aaafe15d894ec5133f0144188';
 const city = 'Tuao';
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat=17.70&lon=121.50&units=imperial&appid=${apiKey}`;
@@ -56,7 +56,43 @@ function displayResults(data) {
     weatherIcon.setAttribute('src', iconsrc);
 
     city.textContent = `${data.name} Municipality, Philippines`;
-    temperature.innerHTML = `Temperature: ${Math.round(data.main.temp)}&deg;F - ${data.weather[0].description}`;
+    temperature.innerHTML = `Temperature: ${Math.round(data.main.temp)}&deg;F - ${capitalizeWords(data.weather[0].description)}`;
+}
+
+function capitalizeWords(str) {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 }
 
 apiFetch();
+
+const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lat=17.70&lon=121.50&units=imperial&appid=${apiKey}`;
+
+async function apiFetchForecast() {
+    try {
+        const response = await fetch(forecast);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data.list);
+            threeDayForecast(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function threeDayForecast(data) {
+    const forecast = data.list.slice(0, 8);
+    const threeDayF = forecast.map(entry => ({date: entry.dt_txt, temperature: (entry.main.temp)}));
+
+    threeDayF.forEach(day => {
+        let figure = document.querySelector('.figForecast');
+        let p = document.createElement('p');
+
+        p.innerHTML = `${day.date}: ${day.temperature}&deg;F`;
+        
+        figure.appendChild(p);
+    });
+}
+apiFetchForecast();
