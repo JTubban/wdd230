7d017d48b584075708d6c8ff3a6f6d51e4e67fd8
jtubban <nakoa024@gmail.com>
2024-03-19 18:03:05 +0800

    page update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -47,32 +47,23 @@ async function apiFetch() {
   }
 }
 
+
+// let latestWeather = document.querySelector(".latestWeather");
+
 function displayResults(data) {
-  let iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let latestWeather = document.querySelector(".latestWeather");
-  let city = document.createElement("h3");
+    let iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let weatherDetails = document.querySelector('.weatherDetails');
 
-  let figure = document.createElement("figure");
-  let img = document.createElement("img");
-  let temperature = document.createElement("p");
+    let city = document.querySelector(".city");
+    let weatherIcon = document.querySelector(".weatherIcon");
+    let temperature = document.querySelector(".temperature");
 
-  let h4 = document.createElement("h4");
-  let div = document.createElement("div");
+    city.textContent = `${data.name} Municipality, Philippines`;
 
-  city.textContent = `${data.name} Municipality, Philippines`;
+    weatherIcon.setAttribute("src", iconsrc);
+    temperature.setAttribute("class", "temperature");
+    temperature.innerHTML = `Temperature: ${Math.round(data.main.temp)}&deg;F - ${capitalizeWords(data.weather[0].description)}`;
 
-  img.setAttribute("src", iconsrc);
-  temperature.setAttribute("class", "temperature");
-  temperature.innerHTML = `Temperature: ${Math.round(data.main.temp)}&deg;F - ${capitalizeWords(data.weather[0].description)}`;
-
-  h4.textContent = "Three-Day Temperature Forecast";
-
-  figure.appendChild(img);
-  figure.appendChild(temperature);
-
-  latestWeather.appendChild(city);
-  latestWeather.appendChild(figure);
-  latestWeather.appendChild(h4);
 }
 
 function capitalizeWords(str) {
@@ -87,38 +78,34 @@ apiFetch();
 const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lat=17.70&lon=121.50&units=imperial&appid=${apiKey}`;
 
 async function apiFetchForecast() {
-  try {
-    const response = await fetch(forecast);
-    if (response.ok) {
-      const data = await response.json();
-      // console.log(data);
-      threeDayForecast(data);
-    } else {
-      throw Error(await response.text());
+    try {
+        const response = await fetch(forecast);
+        if (response.ok) {
+        const data = await response.json();
+        // console.log(data);
+        threeDayForecast(data);
+        } else {
+        throw Error(await response.text());
+        }
+    } catch (error) {
+        error;
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 function threeDayForecast(data) {
-  const forecast = data.list.slice(0, 8);
-  const threeDayF = forecast.map((entry) => ({
-    date: entry.dt_txt,
-    temperature: entry.main.temp,
-  }));
+    const forecast = data.list.slice(0, 8);
+    const threeDayF = forecast.map((entry) => ({
+        date: entry.dt_txt,
+        temperature: entry.main.temp,
+    }));
 
-  threeDayF.forEach((day) => {
-    let latestWeather = document.querySelector(".latestWeather");
-    // let figure = document.querySelector(".figForecast");
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-
-    p.innerHTML = `${day.date}: ${day.temperature}&deg;F`;
-    div.appendChild(p);
-
-    latestWeather.appendChild(div);
-  });
+    threeDayF.forEach((day) => {
+        let forecasts = document.querySelector(".forecasts");
+        let p = document.createElement("p");
+        p.innerHTML = `${day.date}: ${day.temperature}&deg;F`;
+        
+        forecasts.appendChild(p);
+    });
 }
 
 apiFetchForecast();
