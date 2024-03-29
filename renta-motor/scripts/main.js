14bfdc6adcc5641d0c8c5a18f664df2ba4db626c
jtubban <nakoa024@gmail.com>
2024-03-29 08:38:38 +0800

    renta motor project update

---
+++ renta-motor/scripts/main.js
@@ -0,0 +1,17 @@
+// const hamButton = document.querySelector('#menu');
+// const navigation = document.querySelector('.navigation');
+
+// hamButton.addEventListener('click', () => {
+// 	navigation.classList.toggle('open');
+// 	hamButton.classList.toggle('open');
+// });
+
+window.onload = function() {
+    const menuBtn = document.querySelector('.hamburger');
+    const mobileNav = document.querySelector('.mobile-nav');
+
+    menuBtn.addEventListener('click', function() {
+        menuBtn.classList.toggle('is-active');
+        mobileNav.classList.toggle('is-active');
+    });
+}
