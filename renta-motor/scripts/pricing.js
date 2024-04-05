6bd37dc0652e68e8b73eef78bba465a664ec48c5
jtubban <nakoa024@gmail.com>
2024-04-05 15:38:35 +0800

    updated renta motor

--- renta-motor/scripts/pricing.js
+++ renta-motor/scripts/pricing.js
@@ -33,8 +33,10 @@ function displayResults(data) {
         item.setAttribute('class', 'item');
         h2.setAttribute('class', 'item-title');
         pricing.setAttribute('class', 'pricing');
-		book.setAttribute('href', '#');
+		book.setAttribute('href', 'reservations.html');
 		img.setAttribute('src', vehicle.image);
+		img.setAttribute('alt', vehicle.name);
+		img.setAttribute('loading', 'lazy');
 
 		reservation.setAttribute('class','reservation');
 		walkIn.setAttribute('class','walkIn');
@@ -56,7 +58,7 @@ function displayResults(data) {
 			const fullDay1 = document.createElement('p');
 
             reservationTitle.innerText = 'Reservation';
-			halfDay1.innerText = `Half day: ${res.halfDay}`;
+			halfDay1.innerText = `Half day: ${res.halfDay} (3hrs)`;
 			fullDay1.innerText = `Full day: ${res.fullDay}`;
 
             reservation.appendChild(reservationTitle);
@@ -72,7 +74,7 @@ function displayResults(data) {
 			const fullDay2 = document.createElement('p');
 
 			walkInTitle.innerText = 'Walk In';
-			halfDay2.innerText = `Half day: ${walk.halfDay}`;
+			halfDay2.innerText = `Half day: ${walk.halfDay} (3hrs)`;
 			fullDay2.innerText = `Full day: ${walk.fullDay}`;
 
             walkIn.appendChild(walkInTitle);
